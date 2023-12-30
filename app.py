2cb6278f0f7681eeb6a92a7aef45c3564f1ad134
Xeberlhyn <38856291+xeberlhyn@users.noreply.github.com>
2023-12-30 08:38:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,237 @@ def handle_message(event):
             no = 0
             for bokeps in a["data"]:
                 no += 1
-                ret_.append({"type": "bubble", "size": "micro", "body": { "type": "box", "layout": "vertical", "contents": [ { "contents": [ { "contents": [ { "contents": [ { "contents": [ { "type": "image", "url": Xeberlhyn.get_profile(sender).picture_url, "aspectMode": "cover" } ], "type": "box", "layout": "vertical", "borderWidth": "normal", "cornerRadius": "xxl" }, { "contents": [ { "type": "text", "text": "𝐏𝐎𝐑𝐍𝐇𝐔𝐁", "size": "11px", "color": "#66FF00", "align": "center", "weight": "bold", "gravity": "center", "offsetTop": "2px" } ], "type": "box", "layout": "vertical", "borderWidth": "normal", "cornerRadius": "lg", "flex": 5 }, { "contents": [ { "type": "image", "url": "https://i.ibb.co/sCJJXGT/20231226-135250.png", "aspectMode": "cover" } ], "type": "box", "layout": "vertical", "borderWidth": "normal", "cornerRadius": "xxl" } ], "type": "box", "layout": "horizontal", "spacing": "xs" }, { "contents": [ { "contents": [ { "contents": [ { "type": "image", "url": "https://i.ibb.co/pfqtHS5/20220128-040152.png", "size": "full", "offsetTop": "0px", "offsetBottom": "0px", "offsetStart": "0px", "offsetEnd": "0px", "aspectRatio": "4:3", "aspectMode": "cover", "position": "absolute" }, { "contents": [ { "type": "image", "url": "{}" .format(bokeps["image"]), "size": "full", "aspectMode": "cover", "aspectRatio": "4:3"} ], "type": "box", "layout": "vertical" } ], "type": "box", "layout": "vertical" } ], "type": "box", "layout": "vertical", "flex": 4, "spacing": "none" }, { "type": "box", "layout": "vertical", "contents": [ { "type": "text", "text": "{}".format(bokeps["views"]), "size": "11px", "color": "#A9A9A9CC" } ], "justifyContent": "center", "alignItems": "flex-start", "cornerRadius": "xs", "borderWidth": "normal" }, { "contents": [ { "contents": [ { "type": "text", "text": "Durasi: {}" .format(bokeps["duration"]), "size": "11px", "align": "center", "color": "#A9A9A9CC" } ], "type": "box", "layout": "vertical", "width": "89px", "height": "20px", "alignItems": "flex-start", "justifyContent": "center" }, { "contents": [ { "type": "text", "text": "SHARE", "size": "12px", "color": "#FFFF00", "weight": "bold", "align": "center"} ], "type": "box", "layout": "vertical", "borderWidth": "normal", "borderColor": "#00FF00", "cornerRadius": "sm", "width": "40px", "height": "20px", "justifyContent": "center", "alignItems": "center", "backgroundColor": "#191970" } ], "type": "box", "layout": "horizontal", "spacing": "xxl" } ], "type": "box", "layout": "vertical", "spacing": "sm" }, { "type": "separator", "color": "#FFFFFF" }, { "contents": [ { "contents": [ { "url": Xeberlhyn.get_profile("Uab4a2365a6a7a901cb09984f618d36d8").picture_url, "size": "full", "type": "image" } ], "type": "box", "layout": "vertical", "cornerRadius": "lg", "borderWidth": "1px" }, { "contents": [ { "type": "text", "text": "𝐕𝐓𝐄𝐀𝐌 𝐒𝐘𝐒𝐓𝐄𝐌", "size": "12px", "color": "#FFF5EE" } ], "type": "box", "layout": "vertical", "flex": 7, "alignItems": "center", "justifyContent": "center" } ], "type": "box", "layout": "horizontal", "spacing": "sm" } ], "type": "box", "layout": "vertical", "spacing": "sm" } ], "type": "box", "layout": "vertical" } ], "paddingAll": "3px", "borderWidth": "normal", "cornerRadius": "md", "borderColor": "#FF0000" }, "styles": { "body": { "backgroundColor": "#000000" } } })
+                ret_.append({
+  "type": "bubble",
+  "size": "micro",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "contents": [
+          {
+            "contents": [
+              {
+                "contents": [
+                  {
+                    "contents": [
+                      {
+                        "type": "image",
+                        "url": Xeberlhyn.get_profile(sender).picture_url
+                        "aspectMode": "cover"
+                      }
+                    ],
+                    "type": "box",
+                    "layout": "vertical",
+                    "borderWidth": "normal",
+                    "cornerRadius": "xxl"
+                  },
+                  {
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "𝐏𝐎𝐑𝐍𝐇𝐔𝐁",
+                        "size": "11px",
+                        "color": "#66FF00",
+                        "align": "center",
+                        "weight": "bold",
+                        "gravity": "center",
+                        "offsetTop": "2px"
+                      }
+                    ],
+                    "type": "box",
+                    "layout": "vertical",
+                    "borderWidth": "normal",
+                    "cornerRadius": "lg",
+                    "flex": 5
+                  },
+                  {
+                    "contents": [
+                      {
+                        "type": "image",
+                        "url": "https://i.ibb.co/sCJJXGT/20231226-135250.png",
+                        "aspectMode": "cover"
+                      }
+                    ],
+                    "type": "box",
+                    "layout": "vertical",
+                    "borderWidth": "normal",
+                    "cornerRadius": "xxl"
+                  }
+                ],
+                "type": "box",
+                "layout": "horizontal",
+                "spacing": "xs"
+              },
+              {
+                "contents": [
+                  {
+                    "contents": [
+                      {
+                        "contents": [
+                          {
+                            "type": "image",
+                            "url": "https://i.ibb.co/pfqtHS5/20220128-040152.png",
+                            "size": "full",
+                            "offsetTop": "0px",
+                            "offsetBottom": "0px",
+                            "offsetStart": "0px",
+                            "offsetEnd": "0px",
+                            "aspectRatio": "4:3",
+                            "aspectMode": "cover",
+                            "position": "absolute"
+                          },
+                          {
+                            "contents": [
+                              {
+                                "type": "image",
+                                "url": "{}" .format(bokeps["image"]),
+                                "size": "full",
+                                "aspectMode": "cover",
+                                "aspectRatio": "4:3"
+                              }
+                            ],
+                            "type": "box",
+                            "layout": "vertical"
+                          }
+                        ],
+                        "type": "box",
+                        "layout": "vertical"
+                      }
+                    ],
+                    "type": "box",
+                    "layout": "vertical",
+                    "flex": 4,
+                    "spacing": "none"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "Viwersnya",
+                        "size": "11px",
+                        "color": "#A9A9A9CC"
+                      }
+                    ],
+                    "justifyContent": "center",
+                    "alignItems": "flex-start",
+                    "cornerRadius": "xs",
+                    "borderWidth": "normal"
+                  },
+                  {
+                    "contents": [
+                      {
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "Durasi: {}",
+                            "size": "11px",
+                            "align": "center",
+                            "color": "#A9A9A9CC"
+                          }
+                        ],
+                        "type": "box",
+                        "layout": "vertical",
+                        "width": "89px",
+                        "height": "20px",
+                        "alignItems": "flex-start",
+                        "justifyContent": "center"
+                      },
+                      {
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "SHARE",
+                            "size": "12px",
+                            "color": "#FFFF00",
+                            "weight": "bold",
+                            "align": "center"
+                          }
+                        ],
+                        "type": "box",
+                        "layout": "vertical",
+                        "borderWidth": "normal",
+                        "borderColor": "#00FF00",
+                        "cornerRadius": "sm",
+                        "width": "40px",
+                        "height": "20px",
+                        "justifyContent": "center",
+                        "alignItems": "center",
+                        "backgroundColor": "#191970"
+                      }
+                    ],
+                    "type": "box",
+                    "layout": "horizontal",
+                    "spacing": "xxl"
+                  }
+                ],
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm"
+              },
+              {
+                "type": "separator",
+                "color": "#FFFFFF"
+              },
+              {
+                "contents": [
+                  {
+                    "contents": [
+                      {
+                        "url": Xeberlhyn.get_profile("Uab4a2365a6a7a901cb09984f618d36d8").picture_url,
+                        "size": "full",
+                        "type": "image"
+                      }
+                    ],
+                    "type": "box",
+                    "layout": "vertical",
+                    "cornerRadius": "lg",
+                    "borderWidth": "1px"
+                  },
+                  {
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "𝐕𝐓𝐄𝐀𝐌 𝐒𝐘𝐒𝐓𝐄𝐌",
+                        "size": "12px",
+                        "color": "#FFF5EE"
+                      }
+                    ],
+                    "type": "box",
+                    "layout": "vertical",
+                    "flex": 7,
+                    "alignItems": "center",
+                    "justifyContent": "center"
+                  }
+                ],
+                "type": "box",
+                "layout": "horizontal",
+                "spacing": "sm"
+              }
+            ],
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm"
+          }
+        ],
+        "type": "box",
+        "layout": "vertical"
+      }
+    ],
+    "paddingAll": "3px",
+    "borderWidth": "normal",
+    "cornerRadius": "md",
+    "borderColor": "#FF0000"
+  },
+  "styles": {
+    "body": {
+      "backgroundColor": "#000000"
+    }
+  }
+})
             k = len(ret_)//10
             for aa in range(k+1):
                 data = { "type": "carousel", "contents": ret_[aa*10 : (aa+1)*10] } 
