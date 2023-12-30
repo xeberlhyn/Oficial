fded40774f67ab319bed0bc8b3c6579e326642d3
Xeberlhyn <38856291+xeberlhyn@users.noreply.github.com>
2023-12-30 07:54:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -146,41 +146,21 @@ def handle_message(event):
         data =  { "type": "carousel", "contents": [ { "type": "bubble", "size": "deca", "body": { "type": "box", "layout": "vertical", "contents": [ { "type": "box", "layout": "horizontal", "contents": [ { "type": "box", "layout": "vertical", "contents": [ { "type": "image", "url": url, "aspectMode": "cover", "size": "full" } ], "cornerRadius": "100px", "width": "20px", "height": "20px" }, { "type": "box", "layout": "horizontal", "contents": [ { "type": "text", "contents": [], "text": "𝐕𝐓𝐄𝐀𝐌 𝐎𝐅𝐅𝐈𝐂𝐈𝐀𝐋 | 𝙸 𝚊𝚖 𝚛𝚘𝚋𝚘𝚝 𝚕𝚒𝚗𝚎", "size": "10px", "weight": "bold" } ], "justifyContent": "center", "alignItems": "center" } ], "spacing": "md" }, { "type": "separator", "margin": "xs", "color": "#708090" }, { "type": "box", "layout": "horizontal", "contents": [ { "type": "text", "contents": [ { "type": "span", "text": "Jam {}".format(str(y.strftime("%I:%M:%S %p"))), "size": "9px", "weight": "bold", "color": "#778899cc" } ] }, { "type": "text", "contents": [ { "type": "span", "text": "{}".format(str(x.strftime("%A, %d %b %Y"))), "size": "9px", "weight": "bold", "color": "#778899cc" } ], "align": "end" } ] }, { "type": "box", "layout": "vertical", "contents": [ { "type": "text", "text": str(a_), "wrap": True, "size": "10px" } ] }, { "type": "separator", "color": "#708090" }, { "type": "box", "layout": "horizontal", "contents": [ { "type": "box", "layout": "vertical", "contents": [ { "type": "text", "text": "𝐏𝐥𝐞𝐚𝐬𝐞 𝐜𝐥𝐢𝐜𝐤 𝐭𝐨 𝐎𝐰𝐧𝐞𝐫", "size": "10px", "weight": "bold", "decoration": "underline", "color": "#0000FFCC" } ], "justifyContent": "center", "alignItems": "center", "action": { "type": "uri", "uri": "https://line.me/ti/p/~xeberlhyn23" } }, { "type": "box", "layout": "vertical", "contents": [ { "type": "image", "url": Xeberlhyn.get_profile("Uab4a2365a6a7a901cb09984f618d36d8").picture_url, "size": "full", "aspectMode": "cover" } ], "width": "20px", "height": "20px", "borderWidth": "normal", "cornerRadius": "100px" } ], "margin": "sm" } ], "paddingAll": "3px" } } ] }
         Xeberlhyn.reply_message(to, FlexSendMessage(alt_text="©VTEAM-OFFICIAL", contents=data))
 
-    elif VinsenT.startswith("pornhub"):
-        sep = tks.split(" ")
-        query = tks.replace(sep[0]+" ","")
-        link = requests.get("https://lust.scathach.id/pornhub/search?key={}".format(str(query)))
-        a = link.json()
-        profile = Xeberlhyn.get_profile(sender)
-        url = profile.picture_url
-        if a["data"] != []:
-            ret_ = []
-            for bokeps in a["data"]:
-                judul = bokeps["views"]
-                ret_.append({"type": "bubble", "size": "micro", "body": { "type": "box", "layout": "vertical", "contents": [ { "contents": [ { "contents": [ { "contents": [ { "contents": [ { "type": "image", "url": url, "aspectMode": "cover" } ], "type": "box", "layout": "vertical", "borderWidth": "normal", "cornerRadius": "xxl" }, { "contents": [ { "type": "text", "text": "𝐏𝐎𝐑𝐍𝐇𝐔𝐁", "size": "11px", "color": "#66FF00", "align": "center", "weight": "bold", "gravity": "center", "offsetTop": "2px" } ], "type": "box", "layout": "vertical", "borderWidth": "normal", "cornerRadius": "lg", "flex": 5 }, { "contents": [ { "type": "image", "url": "https://i.ibb.co/sCJJXGT/20231226-135250.png", "aspectMode": "cover" } ], "type": "box", "layout": "vertical", "borderWidth": "normal", "cornerRadius": "xxl" } ], "type": "box", "layout": "horizontal", "spacing": "xs" }, { "contents": [ { "contents": [ { "contents": [ { "type": "image", "url": "https://i.ibb.co/pfqtHS5/20220128-040152.png", "size": "full", "offsetTop": "0px", "offsetBottom": "0px", "offsetStart": "0px", "offsetEnd": "0px", "aspectRatio": "4:3", "aspectMode": "cover", "position": "absolute" }, { "contents": [ { "type": "image", "url": "{}" .format(bokeps["image"]), "size": "full", "aspectMode": "cover", "aspectRatio": "4:3"} ], "type": "box", "layout": "vertical" } ], "type": "box", "layout": "vertical" } ], "type": "box", "layout": "vertical", "flex": 4, "spacing": "none" }, { "type": "box", "layout": "vertical", "contents": [ { "type": "text", "text": judul, "size": "11px", "color": "#A9A9A9CC" } ], "justifyContent": "center", "alignItems": "flex-start", "cornerRadius": "xs", "borderWidth": "normal" }, { "contents": [ { "contents": [ { "type": "text", "text": "Durasi: {}" .format(bokeps["duration"]), "size": "11px", "align": "center", "color": "#A9A9A9CC" } ], "type": "box", "layout": "vertical", "width": "89px", "height": "20px", "alignItems": "flex-start", "justifyContent": "center" }, { "contents": [ { "type": "text", "text": "SHARE", "size": "12px", "color": "#FFFF00", "weight": "bold", "align": "center"} ], "type": "box", "layout": "vertical", "borderWidth": "normal", "borderColor": "#00FF00", "cornerRadius": "sm", "width": "40px", "height": "20px", "justifyContent": "center", "alignItems": "center", "backgroundColor": "#191970" } ], "type": "box", "layout": "horizontal", "spacing": "xxl" } ], "type": "box", "layout": "vertical", "spacing": "sm" }, { "type": "separator", "color": "#FFFFFF" }, { "contents": [ { "contents": [ { "url": Xeberlhyn.get_profile("Uab4a2365a6a7a901cb09984f618d36d8").picture_url, "size": "full", "type": "image" } ], "type": "box", "layout": "vertical", "cornerRadius": "lg", "borderWidth": "1px" }, { "contents": [ { "type": "text", "text": "𝐕𝐓𝐄𝐀𝐌 𝐒𝐘𝐒𝐓𝐄𝐌", "size": "12px", "color": "#FFF5EE" } ], "type": "box", "layout": "vertical", "flex": 7, "alignItems": "center", "justifyContent": "center" } ], "type": "box", "layout": "horizontal", "spacing": "sm" } ], "type": "box", "layout": "vertical", "spacing": "sm" } ], "type": "box", "layout": "vertical" } ], "paddingAll": "3px", "borderWidth": "normal", "cornerRadius": "md", "borderColor": "#FF0000" }, "styles": { "body": { "backgroundColor": "#000000" } } })
-            k = len(ret_)//10
-            for aa in range(k+1):
-                okep = { "type": "carousel", "contents": ret_[aa*10 : (aa+1)*10] }
-                Xeberlhyn.reply_message(to, FlexSendMessage(alt_text="©VTEAM-OFFICIAL", contents=okep))
+
 
     elif VinsenT.startswith("idpornhub"):
         sep = tks.split(" ")
         query = tks.replace(sep[0]+" ","")
         link = "https://www.pornhub.com/embed/{}".format(str(query))
-        sendMessage(to,  "• 𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭...!!!\n • 𝑽𝒊𝒅𝒆𝒐 𝒊𝒔 𝒃𝒆𝒊𝒏𝒈 𝒅𝒐𝒘𝒏𝒍𝒐𝒂𝒅𝒆𝒅")
         ydl = youtube_dl.YoutubeDL({'format': 'best', 'outtmpl': 'VteamsBot.mp4'})
         with ydl:
             result = ydl.extract_info(link)
-            try:
-                if 'entries' in result:
-                    video = result['entries'][0]
-                else:
-                    video = result
-                Xeberlhyn.reply_message(to, TextSendMessage(text=video['url']))
-                #preview = "https://i.ibb.co/wrJNNGL/20220219-100319.jpg"
-                #Xeberlhyn.reply_message(to, VideoSendMessage(url, preview))
-            except Exception as e:
-                sendMessage(to, "error")
+            if 'entries' in result:
+                video = result['entries'][0]
+            else:
+                video = result
+            Xeberlhyn.reply_message(to, TextSendMessage(text=video['url']))
+
 #______________________________________________________________________
 @app.route("/callback", methods=['POST'])
 def callback():
